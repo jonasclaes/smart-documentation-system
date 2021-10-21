8e013a3990d9ac20556ddcdf94260198c91989ed
Jonas Claes <jonas@jonasclaes.be>
2021-10-21 14:36:31 +0200

    Client API routes;

---
+++ app/Http/Controllers/Api/ClientController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Client::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $client = Client::create($request->all());
+
+        return response()->json($client, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Client $client)
+    {
+        return $client;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Client $client)
+    {
+        $client->update($request->all());
+
+        return response()->json($client, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Client  $client
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Client $client)
+    {
+        $client->delete();
+
+        return response()->json(null, 204);
+    }
+}
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -9,6 +9,14 @@ class Client extends Model
 {
     use HasFactory;
 
+    // Allow mass filling of the following properties.
+    protected $fillable = [
+        'clientNumber',
+        'name',
+        'contactEmail',
+        'contactPhoneNumber',
+    ];
+
     /**
      * Get the files that the client owns.
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
