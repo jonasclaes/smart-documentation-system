afc45e082aebfaa7d0c27ad2b7b6da002b39e9f3
Jonas Claes <jonas@jonasclaes.be>
2021-10-21 21:54:15 +0200

    Added routes to all the API controllers

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/token/create', [App\Http\Controllers\TokenController::class, 'createToken'])->name('token_create');
 
 Route::apiResources([
-    'client' => App\Http\Controllers\Api\ClientController::class
+    'client' => App\Http\Controllers\Api\ClientController::class,
+    'file' => App\Http\Controllers\Api\FileController::class,
+    'revision' => App\Http\Controllers\Api\RevisionController::class,
+    'revisionRequest' => App\Http\Controllers\Api\RevisionRequestController::class,
+    'document' => App\Http\Controllers\Api\DocumentController::class,
+    'comment' => App\Http\Controllers\Api\CommentController::class,
+    'revisionRequestDocument' => App\Http\Controllers\Api\RevisionRequestDocumentController::class,
+    'revisionRequestComment' => App\Http\Controllers\Api\RevisionRequestCommentController::class,
+    'revisionRequestCategory' => App\Http\Controllers\Api\RevisionRequestCategoryController::class,
+    'QRCode' => App\Http\Controllers\Api\QRCodeController::class,
+    'user' => App\Http\Controllers\Api\UserController::class
 ]);
