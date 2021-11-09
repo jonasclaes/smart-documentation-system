426e0e31b69535a25003e0ad90b1f2f5017e2d11
Jonas Claes <jonas@jonasclaes.be>
2021-11-09 10:52:03 +0100

    Mobile full height fix

--- public/js/app.js
+++ public/js/app.js
@@ -34800,6 +34800,26 @@ app.component("ExampleComponent", _components_ExampleComponent_vue__WEBPACK_IMPO
  */
 
 app.mount('#app');
+var customViewportCorrectionVariable = 'vh';
+
+function setViewportProperty(doc) {
+  var prevClientHeight;
+  var customVar = '--' + (customViewportCorrectionVariable || 'vh');
+
+  function handleResize() {
+    var clientHeight = doc.clientHeight;
+    if (clientHeight === prevClientHeight) return;
+    requestAnimationFrame(function updateViewportHeight() {
+      doc.style.setProperty(customVar, clientHeight * 0.01 + 'px');
+      prevClientHeight = clientHeight;
+    });
+  }
+
+  handleResize();
+  return handleResize;
+}
+
+window.addEventListener('resize', setViewportProperty(document.documentElement));
 
 /***/ }),
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -44,3 +44,25 @@ app.component("ExampleComponent", ExampleComponent);
  */
 
 app.mount('#app');
+
+/**
+ * Full height on mobile fix.
+ */
+var customViewportCorrectionVariable = 'vh';
+
+function setViewportProperty(doc) {
+    var prevClientHeight;
+    var customVar = '--' + ( customViewportCorrectionVariable || 'vh' );
+    function handleResize() {
+        var clientHeight = doc.clientHeight;
+        if (clientHeight === prevClientHeight) return;
+        requestAnimationFrame(function updateViewportHeight(){
+            doc.style.setProperty(customVar, (clientHeight * 0.01) + 'px');
+            prevClientHeight = clientHeight;
+        });
+    }
+    handleResize();
+    return handleResize;
+}
+
+window.addEventListener('resize', setViewportProperty(document.documentElement));
