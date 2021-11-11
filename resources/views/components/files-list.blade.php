911743608af17382c21c044d5a04d8cae8509541
Jonas Claes <jonas@jonasclaes.be>
2021-11-11 19:01:06 +0100

    Mobile layour for files; columns;

--- resources/views/components/files-list.blade.php
+++ resources/views/components/files-list.blade.php
@@ -1,10 +1,10 @@
 @php /** @var App\Models\File[]|Illuminate\Database\Eloquent\Collection $files */ @endphp
 
-<ul>
+<ul class="grid grid-cols-1 lg:grid-cols-2 gap-2">
     @if(count($files) > 0)
         @foreach($files as $file)
             <li>
-                <a href="{{ route('files.show', ['file' => $file->id]) }}" class="flex justify-between bg-white p-3 rounded-xl mb-2 shadow
+                <a href="{{ route('files.show', ['file' => $file->id]) }}" class="flex justify-between bg-white p-3 rounded-xl shadow
                         border border-gray-400 border-opacity-25 hover:bg-gray-200 transition-colors duration-150 ease-in-out items-center">
                     <div>
                         <span>{{ $file->fileId }} - {{ $file->name }}</span>
