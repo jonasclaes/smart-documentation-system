2a6709427d3da51f5e432c3f7b5c2e37b39d4d79
Jonas Claes <jonas@jonasclaes.be>
2021-11-11 18:55:23 +0100

    Updated look and feel of Files page

--- resources/views/components/files-list.blade.php
+++ resources/views/components/files-list.blade.php
@@ -5,17 +5,17 @@
         @foreach($files as $file)
             <li>
                 <a href="{{ route('files.show', ['file' => $file->id]) }}" class="flex justify-between bg-white p-3 rounded-xl mb-2 shadow
-                        border border-gray-400 border-opacity-25 hover:bg-gray-200 transition-colors duration-150 ease-in-out">
+                        border border-gray-400 border-opacity-25 hover:bg-gray-200 transition-colors duration-150 ease-in-out items-center">
                     <div>
-                        <span>{{ $file->fileId }}</span>
-                        <span class="mx-1.5 font-bold">-</span>
-                        <span>{{ $file->client->name }}</span>
-                        <span class="mx-1.5 font-bold">-</span>
-                        <span>{{ $file->name }}</span>
-                        @if($file->enclosureId)
-                            <span class="mx-1.5 font-bold">-</span>
-                            <span>{{ $file->enclosureId }}</span>
-                        @endif
+                        <span>{{ $file->fileId }} - {{ $file->name }}</span>
+                        <br>
+                        <span class="opacity-50">
+                            {{ $file->client->name }}
+                            @if($file->enclosureId)
+                                <span> - {{ $file->enclosureId }}</span>
+                            @endif
+                        </span>
+                        
                     </div>
                     <div>
                         <x-heroicon-o-chevron-right class="h-6 w-6 opacity-25"/>
