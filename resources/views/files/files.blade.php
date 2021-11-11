15fd020c02d47718bf1c6e56e650831a866753b9
Jonas Claes <jonas@jonasclaes.be>
2021-11-11 19:10:29 +0100

    Search files mobile;

--- resources/views/files/files.blade.php
+++ resources/views/files/files.blade.php
@@ -7,23 +7,19 @@
         <div class="bg-white rounded-xl p-4 w-full mb-3">
             <h1 class="text-xl font-semibold mb-2 pb-1 border-b">Files</h1>
             <div class="flex flex-wrap items-start gap-2">
-                <form action="{{ route('files.index') }}" method="GET" class="flex gap-4" id="searchForm">
-                    <div>
-                        <input
-                            type="text"
-                            name="clientName"
-                            class="block px-0.5 border-0 border-b-2 border-gray-300 focus:ring-0 focus:border-black"
-                            placeholder="Client name"
-                            value="{{ old('clientName') }}">
-                    </div>
-                    <div>
-                        <input
-                            type="text"
-                            name="documentName"
-                            class="block px-0.5 border-0 border-b-2 border-gray-300 focus:ring-0 focus:border-black"
-                            placeholder="Document name"
-                            value="{{ old('documentName') }}">
-                    </div>
+                <form action="{{ route('files.index') }}" method="GET" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 w-full" id="searchForm">
+                    <input
+                        type="text"
+                        name="clientName"
+                        class="px-0.5 border-0 border-b-2 border-gray-300 focus:ring-0 focus:border-black flex-grow"
+                        placeholder="Client name"
+                        value="{{ old('clientName') }}">
+                    <input
+                        type="text"
+                        name="documentName"
+                        class="px-0.5 border-0 border-b-2 border-gray-300 focus:ring-0 focus:border-black flex-grow"
+                        placeholder="Document name"
+                        value="{{ old('documentName') }}">
                     <button type="submit" hidden></button>
                 </form>
                 <div class="flex justify-end flex-grow gap-2 w-full md:w-auto">
