2e5de3c47695f438a8dadd1178a638f6acdd42dc
Jonas Claes <jonas@jonasclaes.be>
2021-11-17 10:41:55 +0100

    Converted to translatables

--- resources/views/files/edit.blade.php
+++ resources/views/files/edit.blade.php
@@ -9,7 +9,7 @@
     <div class="container mx-auto px-3">
         <!-- Menu bar -->
         <div class="bg-white rounded-xl p-4 w-full mb-3">
-            <h1 class="text-xl font-semibold">Edit file</h1>
+            <h1 class="text-xl font-semibold">{{ __('Edit file') }}</h1>
         </div>
 
         <!-- Content -->
@@ -19,49 +19,49 @@
                 @method("PUT")
                 <div class="grid grid-cols-1 lg:grid-cols-2 gap-3">
                     <div>
-                        <label for="fileId">File number:</label>
+                        <label for="fileId">{{ __('File number:') }}</label>
                         <input type="text" name="fileId" id="fileId" class="block rounded-md border-0 bg-gray-100 focus:ring-2 w-full"
-                            placeholder="File number" value="{{ old('fileId', $file->fileId) }}">
-                        <small class="opacity-50">Fill in the number of the file here.</small>
+                            placeholder="{{ __('File number') }}" value="{{ old('fileId', $file->fileId) }}">
+                        <small class="opacity-50">{{ __('Fill in the number of the file here.') }}</small>
                         @error('fileId')
                         <br>
                         <small class="text-red-600 font-semibold">{{ $message }}</small>
                         @enderror
                     </div>
                     <div>
-                        <label for="name">File name:</label>
+                        <label for="name">{{ __('File name:') }}</label>
                         <input type="text" name="name" id="name" class="block rounded-md border-0 bg-gray-100 focus:ring-2 w-full"
-                            placeholder="File name" value="{{ old('name', $file->name) }}" required>
-                        <small class="opacity-50">Fill in the name of the file here.</small>
+                            placeholder="{{ __('File name') }}" value="{{ old('name', $file->name) }}" required>
+                        <small class="opacity-50">{{ __('Fill in the name of the file here.') }}</small>
                         @error('name')
                         <br>
                         <small class="text-red-600 font-semibold">{{ $message }}</small>
                         @enderror
                     </div>
                     <div>
-                        <label for="enclosureId">Enclosure ID:</label>
+                        <label for="enclosureId">{{ __('Enclosure ID:') }}</label>
                         <input type="text" name="enclosureId" id="enclosureId" class="block rounded-md border-0 bg-gray-100 focus:ring-2 w-full"
-                            placeholder="Enclosure ID" value="{{ old('enclosureId', $file->enclosureId) }}">
-                        <small class="opacity-50">Fill in the name of the enclosure here.</small>
+                            placeholder="{{ __('Enclosure ID') }}" value="{{ old('enclosureId', $file->enclosureId) }}">
+                        <small class="opacity-50">{{ __('Fill in the name of the enclosure here.') }}</small>
                         @error('enclosureId')
                         <br>
                         <small class="text-red-600 font-semibold">{{ $message }}</small>
                         @enderror
                     </div>
                     <div>
-                        <label for="uniqueId">Unique ID:</label>
+                        <label for="uniqueId">{{ __('Unique ID:') }}</label>
                         <input type="text" name="uniqueId" id="uniqueId" class="block rounded-md border-0 bg-gray-100 focus:ring-2 w-full"
-                            placeholder="Unique ID" value="{{ old('uniqueId', $file->uniqueId) }}">
-                        <small class="opacity-50">Fill in the unique ID of the file here. This ID will be used in the URL generated for the client, as well as in the QR-code.</small>
+                            placeholder="{{ __('Unique ID') }}" value="{{ old('uniqueId', $file->uniqueId) }}">
+                        <small class="opacity-50">{{ __('Fill in the unique ID of the file here. This ID will be used in the URL generated for the client, as well as in the QR-code.') }}</small>
                         <br>
-                        <small class="text-yellow-600"><strong>Warning!</strong> Changing this field directly updates the QR-code, only change this if you know what you're doing!</small>
+                        <small class="text-yellow-600"><strong>{{ __('Warning!') }}</strong> {{ __("Changing this field directly updates the QR-code, only change this if you know what you're doing!") }}</small>
                         @error('uniqueId')
                         <br>
                         <small class="text-red-600 font-semibold">{{ $message }}</small>
                         @enderror
                     </div>
                     <div>
-                        <label for="clientId">Client:</label>
+                        <label for="clientId">{{ __('Client:') }}</label>
                         <select name="clientId" id="clientId" class="block rounded-md border-0 bg-gray-100 focus:ring-2 w-full"
                             placeholder="Client" value="{{ old('clientId', $file->clientId) }}">
                             @foreach ($clients as $client)
@@ -72,7 +72,7 @@
                                 </option>
                             @endforeach
                         </select>
-                        <small class="opacity-50">Select the client of the file here.</small>
+                        <small class="opacity-50">{{ __('Select the client of the file here.') }}</small>
                         @error('clientId')
                         <br>
                         <small class="text-red-600 font-semibold">{{ $message }}</small>
@@ -86,12 +86,12 @@
         <div class="flex justify-end flex-grow gap-2 w-full md:w-auto mt-3">
             <a href="{{ route('files.show', ['file' => $file]) }}"
                 class="bg-red-600 hover:bg-red-700 px-9 py-3 mb-3 text-white rounded inline-flex justify-center items-center">
-                <x-heroicon-o-trash class="h-4 w-4 mr-1" /><span>Discard</span>
+                <x-heroicon-o-trash class="h-4 w-4 mr-1" /><span>{{ __('Discard') }}</span>
             </a>
             <a href="javascript:$('#editForm').submit();"
                 class="bg-green-600 hover:bg-green-700 md:px-9 py-3 mb-3 text-white rounded flex-grow md:flex-grow-0
                 flex justify-center items-center">
-                <x-heroicon-o-pencil class="h-4 w-4 mr-1" /><span>Save</span>
+                <x-heroicon-o-pencil class="h-4 w-4 mr-1" /><span>{{ __('Save') }}</span>
             </a>
         </div>
     </div>
--- resources/views/files/file.blade.php
+++ resources/views/files/file.blade.php
@@ -26,24 +26,24 @@ $qrCodeEncoded = base64_encode($qrCode);
 
         <a href="{{ route('files.index') }}"
             class="bg-gray-700 hover:bg-gray-800 px-9 py-3 mb-3 text-white rounded inline-flex justify-center items-center">
-             <x-heroicon-o-chevron-left class="h-4 w-4"/><span>Back</span>
+             <x-heroicon-o-chevron-left class="h-4 w-4"/><span>{{ __('Back') }}</span>
          </a>
 
         <!-- Menu bar -->
         <div class="bg-white rounded-xl p-4 w-full mb-3">
-            <h1 class="text-xl font-semibold mb-2 pb-1 border-b">File</h1>
+            <h1 class="text-xl font-semibold mb-2 pb-1 border-b">{{ __('File') }}</h1>
             <div class="flex flex-wrap items-start gap-2">
-                <p class="flex-grow"><strong>{{ $file->name }}</strong> made for client <strong>{{ $file->client->name }}</strong></p>
+                <p class="flex-grow"><strong>{{ $file->name }}</strong> {{ __('made for client') }} <strong>{{ $file->client->name }}</strong></p>
                 <div class="flex justify-end flex-grow gap-2 w-full md:w-auto">
                     <a href="{{ route('files.edit', ['file' => $file]) }}"
                        class="bg-blue-600 hover:bg-blue-700 md:px-9 py-3 text-white rounded flex-grow md:flex-grow-0
                        flex justify-center items-center">
-                        <x-heroicon-o-pencil class="h-4 w-4 mr-1"/>Edit
+                        <x-heroicon-o-pencil class="h-4 w-4 mr-1"/>{{ __('Edit') }}
                     </a>
                     <a href="javascript:$('#deleteForm').submit();"
                        class="bg-red-600 hover:bg-red-700 md:px-9 py-3 text-white rounded flex-grow md:flex-grow-0
                        flex justify-center items-center">
-                        <x-heroicon-o-trash class="h-4 w-4 mr-1"/>Delete
+                        <x-heroicon-o-trash class="h-4 w-4 mr-1"/>{{ __('Delete') }}
                     </a>
                 </div>
             </div>
@@ -52,38 +52,38 @@ $qrCodeEncoded = base64_encode($qrCode);
         <!-- Content -->
         <div class="grid gap-3 grid-cols-12">
             <div class="bg-white rounded-xl p-4 col-span-12 md:col-span-6 lg:col-span-5">
-                <h2 class="font-semibold text-lg mb-1">General information</h2>
-                <p>Created on: {{ $file->created_at }}</p>
-                <p>Last edit: {{ $file->updated_at }}</p>
-                <p>File number: {{ $file->fileId }}</p>
-                <p>File name: {{ $file->name }}</p>
+                <h2 class="font-semibold text-lg mb-1">{{ __('General information') }}</h2>
+                <p>{{ __('Created on:') }} {{ $file->created_at }}</p>
+                <p>{{ __('Last edit:') }} {{ $file->updated_at }}</p>
+                <p>{{ __('File number:') }} {{ $file->fileId }}</p>
+                <p>{{ __('File name:') }} {{ $file->name }}</p>
                 @if($file->enclosureId)
-                    <p>Enclosure: {{ $file->enclosureId }}</p>
+                    <p>{{ __('Enclosure:') }} {{ $file->enclosureId }}</p>
                 @endif
-                <p>Unique ID: {{ $file->uniqueId }}</p>
-                <p>This file has been opened {{ $file->QRCode->scanCount }} times.</p>
-                <p class="break-all">Public link: <a href="{{ $file->QRCode->content }}" class="text-blue-500">{{ $file->QRCode->content }}</a></p>
+                <p>{{ __('Unique ID:') }} {{ $file->uniqueId }}</p>
+                <p>{{ trans_choice('This file has been opened once.|This file has been opened :amount times.', $file->QRCode->scanCount, ['amount' => $file->QRCode->scanCount]) }}</p>
+                <p class="break-all">{{ __('Public link:') }} <a href="{{ $file->QRCode->content }}" class="text-blue-500">{{ $file->QRCode->content }}</a></p>
             </div>
             <div class="bg-white rounded-xl p-4 col-span-12 md:col-span-6 lg:col-span-5">
-                <h2 class="font-semibold text-lg mb-1">Client information</h2>
-                <p>Client number: {{ $file->client->clientNumber }}</p>
-                <p>Client name: {{ $file->client->name }}</p>
-                <p>Contact email: <a href="mailto:{{ $file->client->contactEmail }}" class="text-blue-500">{{ $file->client->contactEmail }}</a></p>
-                <p>Contact phone number: <a href="tel:{{ $file->client->contactPhoneNumber }}" class="text-blue-500">{{ $file->client->contactPhoneNumber }}</a></p>
+                <h2 class="font-semibold text-lg mb-1">{{ __('Client information') }}</h2>
+                <p>{{ __('Client number:') }} {{ $file->client->clientNumber }}</p>
+                <p>{{ __('Client name:') }} {{ $file->client->name }}</p>
+                <p>{{ __('Contact email:') }} <a href="mailto:{{ $file->client->contactEmail }}" class="text-blue-500">{{ $file->client->contactEmail }}</a></p>
+                <p>{{ __('Contact phone number:') }} <a href="tel:{{ $file->client->contactPhoneNumber }}" class="text-blue-500">{{ $file->client->contactPhoneNumber }}</a></p>
             </div>
             <div class="bg-white rounded-xl p-4 col-span-12 md:col-span-6 lg:col-span-2">
                 <img id="qr-code" src="data:image/png;base64,{!! $qrCodeEncoded !!}" alt="{{ $file->QRCode->content }}">
                 <a href="javascript:printJS('qr-code', 'html')"
                    class="bg-blue-600 hover:bg-blue-700 md:px-9 py-3 text-white rounded flex justify-center items-center mt-3">
-                    <x-heroicon-o-printer class="h-4 w-4 mr-1"/>Print
+                    <x-heroicon-o-printer class="h-4 w-4 mr-1"/>{{ __('Print') }}
                 </a>
                 <a href="data:image/png;base64,{!! $qrCodeEncoded !!}" download="qr-code.png"
                    class="bg-blue-600 hover:bg-blue-700 md:px-9 py-3 text-white rounded flex justify-center items-center mt-3">
-                    <x-heroicon-o-download class="h-4 w-4 mr-1"/>Download
+                    <x-heroicon-o-download class="h-4 w-4 mr-1"/>{{ __('Download') }}
                 </a>
             </div>
             <div class="bg-white rounded-xl p-4 col-span-12 md:col-span-6 lg:col-span-6 flex flex-col">
-                <h2 class="font-semibold text-lg mb-1">Revisions</h2>
+                <h2 class="font-semibold text-lg mb-1">{{ __('Revisions') }}</h2>
                 @if(count($file->revisions) > 0)
                     <ul>
                         @foreach($file->revisions->sortByDesc('updated_at') as $revision)
@@ -94,7 +94,7 @@ $qrCodeEncoded = base64_encode($qrCode);
                                     <div>
                                         <span>{{ $revision->revisionNumber }}</span>
                                         <br>
-                                        <span class="opacity-50">Updated at: {{ $revision->updated_at }}</span>
+                                        <span class="opacity-50">{{ __('Updated at:') }} {{ $revision->updated_at }}</span>
                                     </div>
                                     <div>
                                         <x-heroicon-o-chevron-right class="h-6 w-6 opacity-25"/>
@@ -105,12 +105,12 @@ $qrCodeEncoded = base64_encode($qrCode);
                     </ul>
                 @else
                     <div class="flex-grow flex items-center">
-                        <span class="text-center w-full">There are currently no revisions.</span>
+                        <span class="text-center w-full">{{ __('There are currently no revisions.') }}</span>
                     </div>
                 @endif
             </div>
             <div class="bg-white rounded-xl p-4 col-span-12 md:col-span-6 lg:col-span-6 flex flex-col">
-                <h2 class="font-semibold text-lg mb-1">Revision requests</h2>
+                <h2 class="font-semibold text-lg mb-1">{{ __('Revision requests') }}</h2>
                 @if(count($file->revisionRequests) > 0)
                     <ul>
                         @foreach($file->revisionRequests->sortByDesc('updated_at') as $revisionRequest)
@@ -119,7 +119,7 @@ $qrCodeEncoded = base64_encode($qrCode);
                                 <a href="#" class="flex justify-between bg-white p-3 rounded-xl mb-2 shadow
                         border border-gray-400 border-opacity-25 hover:bg-gray-200 transition-colors duration-150 ease-in-out items-center">
                                     <div>
-                                        <span class="opacity-50">Updated at: {{ $revisionRequest->updated_at }}</span>
+                                        <span class="opacity-50">{{ __('Updated at:') }} {{ $revisionRequest->updated_at }}</span>
                                     </div>
                                     <div>
                                         <x-heroicon-o-chevron-right class="h-6 w-6 opacity-25"/>
@@ -130,7 +130,7 @@ $qrCodeEncoded = base64_encode($qrCode);
                     </ul>
                 @else
                     <div class="flex-grow flex items-center">
-                        <span class="text-center w-full">There are currently no revision requests.</span>
+                        <span class="text-center w-full">{{ __('There are currently no revision requests.') }}</span>
                     </div>
                 @endif
             </div>
