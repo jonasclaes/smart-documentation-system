188a25e4b4898da5eb6c6b7a2244165909e43fe8
Jonas Claes <jonas@jonasclaes.be>
2021-12-02 17:02:53 +0100

    Translation for users

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -4,26 +4,26 @@
     <div class="container mx-auto px-3">
         <!-- Menu bar -->
         <div class="bg-white rounded-xl p-4 w-full mb-3">
-            <h1 class="text-xl font-semibold mb-2 pb-1 border-b">Users</h1>
+            <h1 class="text-xl font-semibold mb-2 pb-1 border-b">{{ __('Users') }}</h1>
             <div class="flex flex-wrap items-start gap-2">
                 <form action="{{ route('users.index') }}" method="GET" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2 w-full" id="userSearch">
                     <input
                         type="text"
                         name="q"
                         class="px-0.5 border-0 border-b-2 border-gray-300 focus:ring-0 focus:border-black flex-grow"
-                        placeholder="Search User"
+                        placeholder="{{ __('Search user') }}"
                         value="{{old('q')}}">
                 </form>
                 <div class="flex justify-end flex-grow gap-2 w-full md:w-auto">
                     <a href="javascript:$('#userSearch').submit();"
                        class="bg-gray-600 hover:bg-gray-700 md:px-9 py-3 text-white rounded flex-grow md:flex-grow-0
                        flex justify-center items-center">
-                        <x-heroicon-s-search class="h-4 w-4 mr-1"/>Search
+                        <x-heroicon-s-search class="h-4 w-4 mr-1"/>{{ __('Search') }}
                     </a>
                     <a href="{{ route('users.create') }}"
                        class="bg-green-600 hover:bg-green-700 md:px-9 py-3 text-white rounded flex-grow md:flex-grow-0
                        flex justify-center items-center">
-                        <x-heroicon-s-plus class="h-4 w-4 mr-1"/>New
+                        <x-heroicon-s-plus class="h-4 w-4 mr-1"/>{{ __('New') }}
                     </a>
                 </div>
             </div>
